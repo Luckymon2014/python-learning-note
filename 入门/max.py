1a97d776a5b87ecd0909a4b6d3c2fc10f33c4bfb
Luckymon2014 <shan_xiao0823@hotmail.com>
2018-07-06 00:19:16 +0800

    update

---
+++ 入门/max.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# # 模块
+# 
+# - 程序/函数的集合体
+# - 安装模块
+#     - 管理员cmd
+#     - pip install numpy
+
+# #### 定义模块
+
+# In[1]:
+
+
+def func_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+# - File - Download as - Python
+# - max.py
+
+# #### 调用模块
+
+# In[ ]:
+
+
+import max
+
